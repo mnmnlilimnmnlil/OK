f133761c2507ee606293b6819d6f86ce7ccd5b48
Park Mingyu <dbullssg123@naver.com>
2025-10-14 17:20:26 +0900

    ProjectSetingTest01

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Intro from "./pages/Intro";
 import Main from "./pages/Main";
 import SystemBefore from "./pages/SystemBefore";
@@ -11,18 +11,16 @@ import "./styles/global.scss";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/system-before" element={<SystemBefore />} />
-        <Route path="/design-guide" element={<DesignGuide />} />
-        <Route path="/tablet" element={<Tablet />} />
-        <Route path="/smart-watch" element={<SmartWatch />} />
-        <Route path="/kiosk" element={<Kiosk />} />
-        <Route path="/ok-e" element={<OKe />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Intro />} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/system-before" element={<SystemBefore />} />
+      <Route path="/design-guide" element={<DesignGuide />} />
+      <Route path="/tablet" element={<Tablet />} />
+      <Route path="/smart-watch" element={<SmartWatch />} />
+      <Route path="/kiosk" element={<Kiosk />} />
+      <Route path="/ok-e" element={<OKe />} />
+    </Routes>
   );
 }
 
