229db0455e748fb61356c4601de8edc57f987e57
Park Mingyu <dbullssg123@naver.com>
2025-10-14 18:44:54 +0900

    project set api

--- src/pages/OKe/index.jsx
+++ src/pages/OKe/index.jsx
@@ -1,9 +1,14 @@
+import Chat from '../../components/Chat';
 import './style.scss';
 
 export default function OKe() {
   return (
-    <div className='page-container'>
-      <h1>OK-E Page</h1>
+    <div className='ok-e-container'>
+      <div className='ok-e-header'>
+        <h1>OK-E</h1>
+        <p>AI와 대화하며 새로운 아이디어를 탐색해보세요</p>
+      </div>
+      <Chat />
     </div>
   );
 }
