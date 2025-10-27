74b340ad3b77c92b4dc485e31b51cbc23ed781d4
Park Mingyu <dbullssg123@naver.com>
2025-10-27 22:20:47 +0900

    10-27

--- src/pages/Kiosk/index.jsx
+++ src/pages/Kiosk/index.jsx
@@ -1,9 +1,15 @@
 import styles from './style.module.scss';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default function Kiosk() {
   return (
-    <div className={styles.pageContainer}>
-      <h1>키오스크</h1>
-    </div>
+    <>
+      <Header />
+      <div className={styles.pageContainer}>
+        <h1>키오스크</h1>
+      </div>
+      <Footer />
+    </>
   );
 }
